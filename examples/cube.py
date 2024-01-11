a75e5b97c3e359f6f93fc2f4c16c0fa41a0d7d8e
Mattia-Vicari <mattia.vic99@gmail.com>
2024-01-12 00:44:34 +0100

    update example

--- examples/cube.py
+++ examples/cube.py
@@ -1,14 +1,13 @@
 import py3dgame as p3g
 import pygame
 
-width, height = 800, 600
+width, height = 1080, 720
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((width, height))
     clock = pygame.time.Clock()
     fps = 60
-    font = pygame.font.SysFont('arial', 18, True)
 
     color = (
         p3g.color.RED,
@@ -20,20 +19,23 @@ def main():
     )
 
     scene = p3g.Scene(p3g.color.BLACK)
-    scene.add_body(p3g.Body.cube("cube", 10, color=color))
+    cube = p3g.Body.cube("cube", 10, color=color)
+    scene.add_body(cube)
     camera = p3g.Camera(p3g.Vec3(-50, 0, 15), p3g.Vec3(1, 0, -0.3))
 
+    renderer = p3g.Renderer(screen, camera, scene, clock)
     run = True
+
     while run:
         clock.tick(fps)
-        real_fps = clock.get_fps()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
-        scene.bodies["cube"].rotate_deg(1)
+        cube.rotate_deg(1)
 
-        p3g.render(screen, scene, camera, real_fps, font)
+        renderer.render()
 
     pygame.quit()
 

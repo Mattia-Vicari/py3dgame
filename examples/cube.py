c5b0786956bc4ea5e8efd7f5957cf63ee5caef73
Mattia-Vicari <mattia.vic99@gmail.com>
2024-01-11 17:41:30 +0100

    update examples to new color module

--- examples/cube.py
+++ examples/cube.py
@@ -11,16 +11,16 @@ def main():
     font = pygame.font.SysFont('arial', 18, True)
 
     color = (
-        p3g.Color.red,
-        p3g.Color.green,
-        p3g.Color.blue,
-        p3g.Color.purple,
-        p3g.Color.yellow,
-        p3g.Color.cyan
+        p3g.color.RED,
+        p3g.color.GREEN,
+        p3g.color.BLUE,
+        p3g.color.PURPLE,
+        p3g.color.YELLOW,
+        p3g.color.CYAN
     )
 
-    scene = p3g.Scene(p3g.Color.black)
-    scene.add_body(p3g.Body.cube(10, color=color))
+    scene = p3g.Scene(p3g.color.BLACK)
+    scene.add_body(p3g.Body.cube("cube", 10, color=color))
     camera = p3g.Camera(p3g.Vec3(-50, 0, 15), p3g.Vec3(1, 0, -0.3))
 
     run = True
@@ -31,7 +31,7 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-        scene.bodies[0].rotate_deg(1)
+        scene.bodies["cube"].rotate_deg(1)
 
         p3g.render(screen, scene, camera, real_fps, font)
 

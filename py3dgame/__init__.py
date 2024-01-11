52c1c9f07fbeb51b2c5e87e4de413d3eff7a6c52
Mattia-Vicari <mattia.vic99@gmail.com>
2024-01-11 10:56:33 +0100

    adjust __init__ imports

--- py3dgame/__init__.py
+++ py3dgame/__init__.py
@@ -1,4 +1,8 @@
-from body import Body
-from renderer import *
-from scene import *
-from vec import Vec
+"""
+Easy to use 3d engine based on pygame.
+"""
+
+from .color import Color
+from .rendering import render, Camera
+from .scene import Body, Scene
+from .math3d import Vec, Vec3, Quat
